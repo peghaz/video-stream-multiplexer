89a3107f56a6fd80d1ae01c665f00467b7d9d7da
Mehdi <mehdi.yaminli@a2z.az>
2025-03-14 11:28:06 +0400

    Fixed gstreamer error in linking

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <ffmpeg/libavutil/avutil.h>
+#include <gst/gst.h>
 
 void print_avutil_version()
 {
     printf("FFmpeg avutil version: %s\n", av_version_info());
 }
 
-int main(int, char **)
+int main(int argc, char **argv)
 {
+
+    gst_init(&argc, &argv);
+
+    const gchar *nano_str;
+    guint major, minor, micro, nano;
+
+    gst_version(&major, &minor, &micro, &nano);
+    nano_str = (nano == 1) ? "(CVS)" : (nano == 2) ? "(Prerelease)"
+                                                   : "";
+
+    g_print("GStreamer version: %d.%d.%d %s\n", major, minor, micro, nano_str);
     print_avutil_version();
 }
