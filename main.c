81099c080155f84705216a0b18ec33c8b0b89816
Mehdi <mehdi.yaminli@a2z.az>
2025-03-15 19:44:36 +0400

    Wrote README.MD

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 
 int main(int argc, char *argv[])
 {
-    StreamConfigList *config_list = parse_stream_configs_from_json("config/streams.json");
+    char *config_path;
+
+    if (argc == 2)
+        config_path = argv[1];
+    else
+        config_path = "config/streams.json";
+
+    StreamConfigList *config_list = parse_stream_configs_from_json(config_path);
     initialize_multiple_streams(config_list);
     return 0;
 }
